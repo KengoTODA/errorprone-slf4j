5481982428f23b50b52fdc20860da1287c3fd5d4
Kengo TODA <skypencil@gmail.com>
2018-06-30 16:51:55 +0800

    add a bug pattern that prefers final logger field

---
+++ src/test/java/jp/skypencil/errorprone/slf4j/PreferFinalSlf4jLoggerTest.java
@@ -0,0 +1,35 @@
+package jp.skypencil.errorprone.slf4j;
+
+import com.google.errorprone.BugCheckerRefactoringTestHelper;
+import com.google.errorprone.BugCheckerRefactoringTestHelper.TestMode;
+import com.google.errorprone.bugpatterns.BugChecker;
+import java.io.IOException;
+import org.junit.Test;
+
+public class PreferFinalSlf4jLoggerTest {
+  @Test
+  public void testRefactoringStaticLogger() throws IOException {
+    BugChecker checker = new PreferFinalSlf4jLogger();
+    BugCheckerRefactoringTestHelper helper =
+        BugCheckerRefactoringTestHelper.newInstance(checker, getClass());
+    helper
+        .addInputLines(
+            "NonFinalLogger.java",
+            "import org.slf4j.Logger;\n"
+                + "import org.slf4j.LoggerFactory;\n"
+                + "\n"
+                + "public class NonFinalLogger {\n"
+                + "    private Logger logger = LoggerFactory.getLogger(getClass());\n"
+                + "}")
+        .addOutputLines(
+            "NonFinalLogger.java",
+            "import org.slf4j.Logger;\n"
+                + "import org.slf4j.LoggerFactory;\n"
+                + "\n"
+                + "public class NonFinalLogger {\n"
+                + "    private final Logger logger = LoggerFactory.getLogger(getClass());\n"
+                + "}\n"
+                + "")
+        .doTest(TestMode.TEXT_MATCH);
+  }
+}

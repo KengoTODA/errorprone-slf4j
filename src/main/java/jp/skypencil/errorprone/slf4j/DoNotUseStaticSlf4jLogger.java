0189caa165e6a447ad9db2630faae49c361cc0df
Kengo TODA <skypencil@gmail.com>
2018-07-04 11:53:49 +0800

    apply SuggestedFixes utility to shorten code

--- src/main/java/jp/skypencil/errorprone/slf4j/DoNotUseStaticSlf4jLogger.java
+++ src/main/java/jp/skypencil/errorprone/slf4j/DoNotUseStaticSlf4jLogger.java
@@ -6,17 +6,18 @@ import static com.google.errorprone.matchers.Matchers.isField;
 import static com.google.errorprone.matchers.Matchers.isStatic;
 
 import com.google.auto.service.AutoService;
+import com.google.common.base.CaseFormat;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.VariableTreeMatcher;
-import com.google.errorprone.fixes.Fix;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.fixes.SuggestedFix.Builder;
+import com.google.errorprone.fixes.SuggestedFixes;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
-import com.sun.source.tree.ModifiersTree;
 import com.sun.source.tree.VariableTree;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Optional;
 import javax.lang.model.element.Modifier;
 
 @BugPattern(
@@ -28,32 +29,47 @@ import javax.lang.model.element.Modifier;
 public class DoNotUseStaticSlf4jLogger extends BugChecker implements VariableTreeMatcher {
   private static final long serialVersionUID = 2656759159827947106L;
   private static final Matcher<VariableTree> SLF4J_LOGGER = new LoggerMatcher();
-  private final VariableGenerator generator = new VariableGenerator();
 
   @Override
   public Description matchVariable(VariableTree tree, VisitorState state) {
     if (allOf(isField(), SLF4J_LOGGER, isStatic()).matches(tree, state)) {
-      Fix fix =
-          generator.createSuggestedFix(
-              state,
-              tree,
-              createSuggestedFlags(tree.getModifiers()),
-              "LoggerFactory.getLogger(getClass())");
+      Builder builder = SuggestedFix.builder();
+      SuggestedFixes.removeModifiers(tree, state, Modifier.STATIC).ifPresent(builder::merge);
+      suggestRename(tree, state).ifPresent(builder::merge);
+      suggestInitializer(tree, state).ifPresent(builder::merge);
+
       return Description.builder(
               tree,
               "Slf4jLoggerShouldBeNonStatic",
               "https://github.com/KengoTODA/findbugs-slf4j#slf4j_logger_should_be_non_static",
               SUGGESTION,
               "Do not use static Logger field, use non-static one instead")
-          .addFix(fix)
+          .addFix(builder.build())
           .build();
     }
     return Description.NO_MATCH;
   }
 
-  private Set<Modifier> createSuggestedFlags(ModifiersTree modifiers) {
-    Set<Modifier> flags = new HashSet<>(modifiers.getFlags());
-    flags.remove(Modifier.STATIC);
-    return flags;
+  private static final String SUGGESTED_INITIALIZER = "LoggerFactory.getLogger(getClass())";
+
+  private Optional<SuggestedFix> suggestInitializer(VariableTree tree, VisitorState state) {
+    String initializer = tree.getInitializer().toString();
+    if (initializer.equals(SUGGESTED_INITIALIZER)) {
+      return Optional.empty();
+    }
+    return Optional.of(
+        SuggestedFix.builder()
+            .replace(tree.getInitializer(), SUGGESTED_INITIALIZER)
+            .addImport("org.slf4j.LoggerFactory")
+            .build());
+  }
+
+  private Optional<SuggestedFix> suggestRename(VariableTree tree, VisitorState state) {
+    String name = tree.getName().toString();
+    String formatted = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, name);
+    if (name.equals(formatted)) {
+      return Optional.empty();
+    }
+    return Optional.of(SuggestedFixes.renameVariable(tree, formatted, state));
   }
 }

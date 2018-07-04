d177d36d7dc52f59c5299fa63af3ccc06e395018
Kengo TODA <skypencil@gmail.com>
2018-07-04 17:25:47 +0800

    improve coverage

--- src/test/java/jp/skypencil/errorprone/slf4j/IllegalPassedClassTest.java
+++ src/test/java/jp/skypencil/errorprone/slf4j/IllegalPassedClassTest.java
@@ -3,6 +3,7 @@ package jp.skypencil.errorprone.slf4j;
 import com.google.errorprone.BugCheckerRefactoringTestHelper;
 import com.google.errorprone.BugCheckerRefactoringTestHelper.FixChoosers;
 import com.google.errorprone.BugCheckerRefactoringTestHelper.TestMode;
+import com.google.errorprone.CompilationTestHelper;
 import com.google.errorprone.bugpatterns.BugChecker;
 import java.io.IOException;
 import org.junit.Test;
@@ -115,5 +116,24 @@ public class IllegalPassedClassTest {
                 + "}\n"
                 + "")
         .doTest(TestMode.TEXT_MATCH);
+  }
+
+  @Test
+  public void testOtherField() throws IOException {
+    CompilationTestHelper helper =
+        CompilationTestHelper.newInstance(IllegalPassedClass.class, getClass());
+    helper
+        .addSourceLines(
+            "WithLoggerFactory.java",
+            "import org.slf4j.Logger;\n"
+                + "import org.slf4j.ILoggerFactory;\n"
+                + "import org.slf4j.LoggerFactory;\n"
+                + "\n"
+                + "public class WithLoggerFactory {\n"
+                + "    private final String HELLO = \"World\";"
+                + "    private final ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();\n"
+                + "    private final Logger logger = LoggerFactory.getLogger(\"string\");\n"
+                + "}")
+        .expectNoDiagnostics();
   }
 }

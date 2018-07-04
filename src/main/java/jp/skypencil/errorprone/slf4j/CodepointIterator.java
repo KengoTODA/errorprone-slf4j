5e7c4e27dfc2bd376c73c7e9487338b23ff09ffc
Kengo TODA <skypencil@gmail.com>
2018-07-05 08:26:31 +0900

    add new Slf4jSignOnlyFormat bug pattern

---
+++ src/main/java/jp/skypencil/errorprone/slf4j/CodepointIterator.java
@@ -0,0 +1,35 @@
+package jp.skypencil.errorprone.slf4j;
+
+import java.util.Iterator;
+
+final class CodepointIterator implements Iterator<Integer> {
+  private CharSequence sequence;
+  private int index;
+
+  CodepointIterator(CharSequence sequence) {
+    this.sequence = sequence;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return index < sequence.length();
+  }
+
+  @Override
+  public Integer next() {
+    final int result;
+    if (Character.isHighSurrogate(sequence.charAt(index))) {
+      result = (sequence.charAt(index) << 16) + sequence.charAt(index + 1);
+      index += 2;
+    } else {
+      result = sequence.charAt(index);
+      index += 1;
+    }
+    return Integer.valueOf(result);
+  }
+
+  @Override
+  public void remove() {
+    throw new UnsupportedOperationException();
+  }
+}
---
+++ src/main/java/jp/skypencil/errorprone/slf4j/SignOnlyFormat.java
@@ -0,0 +1,88 @@
+package jp.skypencil.errorprone.slf4j;
+
+import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+
+import com.google.auto.service.AutoService;
+import com.google.common.collect.ImmutableSet;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import com.google.errorprone.matchers.CompileTimeConstantExpressionMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.method.MethodMatchers;
+import com.google.errorprone.matchers.method.MethodMatchers.MethodNameMatcher;
+import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.tools.javac.code.Symbol.VarSymbol;
+import com.sun.tools.javac.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+@BugPattern(
+    name = "Slf4jSignOnlyFormat",
+    summary = "To make log readable, log format should contain not only sign but also texts",
+    tags = {"SLF4J"},
+    severity = ERROR)
+@AutoService(BugChecker.class)
+public class SignOnlyFormat extends BugChecker implements MethodInvocationTreeMatcher {
+  private static final long serialVersionUID = 3271269614137732880L;
+  private static final ImmutableSet<String> TARGET_METHOD_NAMES =
+      ImmutableSet.of("trace", "debug", "info", "warn", "error");
+
+  private static final CompileTimeConstantExpressionMatcher IS_CONST =
+      new CompileTimeConstantExpressionMatcher();
+  private static final MethodNameMatcher IS_LOGGING_METHOD =
+      MethodMatchers.instanceMethod()
+          .onDescendantOf("org.slf4j.Logger")
+          .withNameMatching(
+              Pattern.compile(
+                  TARGET_METHOD_NAMES
+                      .stream()
+                      .map(Object::toString)
+                      .collect(Collectors.joining("|"))));
+
+  @Override
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    if (!IS_LOGGING_METHOD.matches(tree, state)) {
+      return Description.NO_MATCH;
+    }
+
+    List<VarSymbol> parameters = ASTHelpers.getSymbol(tree).getParameters();
+    int formatIndex =
+        ASTHelpers.isSubtype(
+                parameters.get(0).type, state.getTypeFromString("org.slf4j.Marker"), state)
+            ? 1
+            : 0;
+
+    if (!IS_CONST.matches(tree.getArguments().get(formatIndex), state)) {
+      return Description.NO_MATCH;
+    }
+
+    String format = ASTHelpers.constValue(tree.getArguments().get(formatIndex)).toString();
+    if (verifyFormat(format)) {
+      return Description.NO_MATCH;
+    }
+    String message =
+        String.format(
+            "SLF4J logging format should contain non-sign text, but it is \'%s\'", format);
+    return Description.builder(
+            tree,
+            "Slf4jSignOnlyFormat",
+            "https://github.com/KengoTODA/findbugs-slf4j#slf4j_sign_only_format",
+            ERROR,
+            message)
+        .build();
+  }
+
+  private boolean verifyFormat(String formatString) {
+    CodepointIterator iterator = new CodepointIterator(formatString);
+    while (iterator.hasNext()) {
+      if (Character.isLetter(iterator.next().intValue())) {
+        // found non-sign character.
+        return true;
+      }
+    }
+    return false;
+  }
+}

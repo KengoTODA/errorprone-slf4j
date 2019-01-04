e15b478a30f8a80012120c163b0590e48d54e529
Kengo TODA <skypencil@gmail.com>
2019-01-04 11:42:01 +0800

    bump up JDK to 11

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module jp.skypencil.errorprone.slf4j {
+  requires java.base;
+  requires java.compiler;
+  requires jdk.compiler;
+  requires error.prone.annotation;
+  requires error.prone.check.api;
+  requires com.google.common;
+  requires auto.service;
+}

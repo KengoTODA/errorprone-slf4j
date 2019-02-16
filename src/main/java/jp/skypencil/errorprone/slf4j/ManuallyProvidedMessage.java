f883e58a1f22efc616b12833587b799c35d48c05
Kengo TODA <skypencil@gmail.com>
2019-02-16 08:04:54 +0800

    apply format by spotless

--- src/main/java/jp/skypencil/errorprone/slf4j/ManuallyProvidedMessage.java
+++ src/main/java/jp/skypencil/errorprone/slf4j/ManuallyProvidedMessage.java
@@ -2,9 +2,6 @@ package jp.skypencil.errorprone.slf4j;
 
 import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
 
-import java.util.Optional;
-import java.util.function.Predicate;
-
 import com.google.auto.service.AutoService;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
@@ -16,42 +13,57 @@ import com.sun.source.tree.MethodInvocationTree;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.tree.JCTree.JCFieldAccess;
 import com.sun.tools.javac.tree.JCTree.JCMethodInvocation;
+import java.util.Optional;
+import java.util.function.Predicate;
 
-@BugPattern(name = "Slf4jDoNotLogMessageOfExceptionExplicitly", summary = "Do not log message returned from Throwable#getMessage and Throwable#getLocalizedMessage", tags = {
-		"SLF4J" }, severity = ERROR)
+@BugPattern(
+    name = "Slf4jDoNotLogMessageOfExceptionExplicitly",
+    summary =
+        "Do not log message returned from Throwable#getMessage and Throwable#getLocalizedMessage",
+    tags = {"SLF4J"},
+    severity = ERROR)
 @AutoService(BugChecker.class)
 public class ManuallyProvidedMessage extends BugChecker implements MethodInvocationTreeMatcher {
 
-	private static final long serialVersionUID = 7903613628689308557L;
-	private static final Predicate<String> MESSAGE_GETTER = "getMessage"::equals;
-	private static final Predicate<String> LOCALIZED_MESSAGE_GETTER = "getLocalizedMessage"::equals;
-	private static final Predicate<String> THROWABLE = "java.lang.Throwable"::equals;
+  private static final long serialVersionUID = 7903613628689308557L;
+  private static final Predicate<String> MESSAGE_GETTER = "getMessage"::equals;
+  private static final Predicate<String> LOCALIZED_MESSAGE_GETTER = "getLocalizedMessage"::equals;
+  private static final Predicate<String> THROWABLE = "java.lang.Throwable"::equals;
 
-	@Override
-	public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
-		Symbol method = ASTHelpers.getSymbol(tree.getMethodSelect());
-		String methodStr = method.toString();
-		String methodName = methodStr.substring(0, methodStr.indexOf('('));
-		if (!Consts.TARGET_METHOD_NAMES.contains(methodName)) {
-			return Description.NO_MATCH;
-		}
+  @Override
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    Symbol method = ASTHelpers.getSymbol(tree.getMethodSelect());
+    String methodStr = method.toString();
+    String methodName = methodStr.substring(0, methodStr.indexOf('('));
+    if (!Consts.TARGET_METHOD_NAMES.contains(methodName)) {
+      return Description.NO_MATCH;
+    }
 
-		Symbol clazz = method.enclClass();
-		if (!"org.slf4j.Logger".equals(clazz.toString())) {
-			return Description.NO_MATCH;
-		}
-		Optional<JCFieldAccess> problem = tree.getArguments().stream()
-				.filter(arg -> arg.getClass().isAssignableFrom(JCMethodInvocation.class))
-				.map(JCMethodInvocation.class::cast).map(arg -> arg.meth)
-				.filter(meth -> meth.getClass().isAssignableFrom(JCFieldAccess.class)).map(JCFieldAccess.class::cast)
-				.filter(meth -> MESSAGE_GETTER.or(LOCALIZED_MESSAGE_GETTER).test(meth.sym.name.toString()))
-				.filter(meth -> THROWABLE.test(meth.sym.owner.toString())).findFirst();
-		if (problem.isPresent()) {
-			return Description.builder(tree, "Slf4jDoNotLogMessageOfExceptionExplicitly",
-					"https://github.com/KengoTODA/findbugs-slf4j#slf4j_manually_provided_message", ERROR,
-					"Do not log message returned from Throwable#getMessage and Throwable#getLocalizedMessage. It is enough to provide throwable instance as the last argument, then binding will log its message.")
-					.build();
-		}
-		return Description.NO_MATCH;
-	}
+    Symbol clazz = method.enclClass();
+    if (!"org.slf4j.Logger".equals(clazz.toString())) {
+      return Description.NO_MATCH;
+    }
+    Optional<JCFieldAccess> problem =
+        tree.getArguments()
+            .stream()
+            .filter(arg -> arg.getClass().isAssignableFrom(JCMethodInvocation.class))
+            .map(JCMethodInvocation.class::cast)
+            .map(arg -> arg.meth)
+            .filter(meth -> meth.getClass().isAssignableFrom(JCFieldAccess.class))
+            .map(JCFieldAccess.class::cast)
+            .filter(
+                meth -> MESSAGE_GETTER.or(LOCALIZED_MESSAGE_GETTER).test(meth.sym.name.toString()))
+            .filter(meth -> THROWABLE.test(meth.sym.owner.toString()))
+            .findFirst();
+    if (problem.isPresent()) {
+      return Description.builder(
+              tree,
+              "Slf4jDoNotLogMessageOfExceptionExplicitly",
+              "https://github.com/KengoTODA/findbugs-slf4j#slf4j_manually_provided_message",
+              ERROR,
+              "Do not log message returned from Throwable#getMessage and Throwable#getLocalizedMessage. It is enough to provide throwable instance as the last argument, then binding will log its message.")
+          .build();
+    }
+    return Description.NO_MATCH;
+  }
 }

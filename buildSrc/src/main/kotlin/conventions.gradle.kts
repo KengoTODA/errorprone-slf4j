f213c93ab8fba0d06ddcf5804cfc22d6e28acd04
Kengo TODA <skypencil+github@gmail.com>
2022-01-30 12:53:05 +0800

    build: split dependency managements to enable automated patch release with dependabot
    * build: split non-runtime dependencies into buildSrc dir
    
    close #145
    
    * chore: keep the binary compatibility with prev release
    
    * build: skip analysis on files in the build dir

---
+++ buildSrc/src/main/kotlin/conventions.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    `java`
+    id("com.diffplug.spotless")
+    id("io.github.gradle-nexus.publish-plugin")
+    id("net.ltgt.errorprone")
+    id("org.sonarqube")
+}
+
+val autoServiceVersion = "1.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    annotationProcessor("com.google.auto.service:auto-service:$autoServiceVersion")
+    compileOnly("com.google.auto.service:auto-service:$autoServiceVersion")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.slf4j:slf4j-api:2.0.0-alpha6")
+}
+
+spotless {
+    java {
+        removeUnusedImports()
+        googleJavaFormat()
+    }
+    kotlinGradle {
+        target("build.gradle.kts")
+        ktlint()
+        indentWithSpaces()
+    }
+}
+
+sonarqube {
+    properties {
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.projectKey", "jp.skypencil.errorprone.slf4j:errorprone-slf4j")
+        property("sonar.organization", "kengotoda-github")
+    }
+}

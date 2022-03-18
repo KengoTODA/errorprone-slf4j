9ac660bed6014ffe57311042d0b837a8fc2fa4f6
Kengo TODA <skypencil@gmail.com>
2022-03-18 08:29:50 +0800

    build: remove the gradle enterprise plugin
    It triggers a release by dependabot unexpectedly, so remove it from
    the settings file.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,1 @@
-plugins { id("com.gradle.enterprise") version "3.9" }
-
 rootProject.name = "errorprone-slf4j"
-
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-    }
-}
